86eb2102ed99526bb2313cbec627763f19f40990
VivekUdupa <vkoodli@g.clmeson.edu>
2021-01-01 23:19:11 -0500

    start

---
+++ Convolution/conv.c
@@ -0,0 +1,15 @@
+/* This program is a C implementation of a 7x7 mean filter 
+Author: Vivek Koodli Udupa
+Class: Clemson Intro to Computer Vision by Adam Hoover 
+Purpose: Rewriting all codes from CV class
+Date: Jan 1 2021
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	
+}
